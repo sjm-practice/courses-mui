4173bad6337d5f916f206e27328a51680181a244
stevenjmarsh <sjm.atl@gmail.com>
2018-06-24 15:09:47 -0400

    installing react

---
+++ imports/ui/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+// App component - represents the whole app
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Hello React World!</h1>
+        </header>
+      </div>
+    );
+  }
+}
